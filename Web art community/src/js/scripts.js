4351e285e0e784ebc694bf4fd00e5fdfd3c6c1ea
kimdien189 <109838608+kimdien189@users.noreply.github.com>
2023-06-26 08:09:50 +0700

    web art project update

--- Web art community/src/js/scripts.js
+++ Web art community/src/js/scripts.js
@@ -1,21 +1,14 @@
-$(document).ready(function(){
-    $("#search").focus(function() {
-      $(".search-box").addClass("border-searching");
-      $(".search-icon").addClass("si-rotate");
-    });
-    $("#search").blur(function() {
-      $(".search-box").removeClass("border-searching");
-      $(".search-icon").removeClass("si-rotate");
-    });
-    $("#search").keyup(function() {
-        if($(this).val().length > 0) {
-          $(".go-icon").addClass("go-in");
-        }
-        else {
-          $(".go-icon").removeClass("go-in");
-        }
-    });
-    $(".go-icon").click(function(){
-      $(".search-form").submit();
-    });
+const toTopBtn = document.getElementById("to-top-btn");
+
+toTopBtn.addEventListener("click", function() {
+  console.log("Button clicked!");
+  document.documentElement.scrollTop = 0;
+});
+
+window.addEventListener("scroll", function() {
+  if (document.documentElement.scrollTop > 200) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
 });
